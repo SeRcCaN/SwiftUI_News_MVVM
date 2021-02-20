8d182d419192f924ca2d4ebb1fb72a1d2b82aec1
Sercan Burak AĞIR <sercanburak@gmail.com>
2021-02-20 13:05:26 +0300

    Initial Commit

---
+++ NewsMVVM/NewsMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsMVVMApp.swift
+//  NewsMVVM
+//
+//  Created by Sercan Burak AĞIR on 20.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
